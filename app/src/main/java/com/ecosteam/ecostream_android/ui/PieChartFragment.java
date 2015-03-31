955adff2ffe0dfd4df792796074bbe4e99e74e19
Tianchi Liu <ltcwendell@gmail.com>
2015-03-30 22:23:27 -0700

    added color change

--- app/src/main/java/com/ecosteam/ecostream_android/ui/PieChartFragment.java
+++ app/src/main/java/com/ecosteam/ecostream_android/ui/PieChartFragment.java
@@ -30,10 +30,13 @@ public class PieChartFragment extends Fragment implements RetrieveDataCallback {
     private PieModel mPositive;
     private PieModel mNegative;
     private Timer mTimer;
+    private boolean mIsAlertShowing;
+    private View mBackground;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pie_chart, container, false);
+        mBackground = view;
         mSensorId = getArguments().getInt(MainFragment.KEY_SENSOR_ID, -1);
 
         if (mSensorId == R.id.sensor_1) {
@@ -55,10 +58,17 @@ public class PieChartFragment extends Fragment implements RetrieveDataCallback {
         mPieChart.startAnimation();
 
         mLabel = (TextView) view.findViewById(R.id.label);
-        mLabel.setText("0");
+        mLabel.setText("0% flow rate");
 
+        mIsAlertShowing = false;
         mTimer = new Timer();
-        mTimer.scheduleAtFixedRate(new TimerTask() {
+        startTimer(mTimer);
+
+        return view;
+    }
+
+    private void startTimer(Timer t) {
+        t.scheduleAtFixedRate(new TimerTask() {
             synchronized public void run() {
                 if (mSensorId == R.id.sensor_1) {
                     ParseDataFactory.retrieveData(Constant.SENSOR_1_TABLE, 1, PieChartFragment.this);
@@ -71,14 +81,14 @@ public class PieChartFragment extends Fragment implements RetrieveDataCallback {
                 }
             }
         }, 0, Constant.FETCHING_RATE_MILLIS);
-
-        return view;
     }
+
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         mTimer.cancel();
+        mTimer = null;
     }
 
     @Override
@@ -92,9 +102,19 @@ public class PieChartFragment extends Fragment implements RetrieveDataCallback {
     }
 
     private void updatePieChar(int val) {
+        if (val < 0 || val > 100) {
+            return;
+        }
+
         mPositive.setValue(val);
         mNegative.setValue(100 - val);
         mPieChart.update();
         mLabel.setText(val + "% flow rate");
+
+        if (val > 70) {
+            mPositive.setColor(Color.parseColor("#FF0000"));
+        } else {
+            mPositive.setColor(Color.parseColor("#56B7F1"));
+        }
     }
 }
